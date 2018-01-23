bfbb31cded420c094bb202485217dacb900713f0
fengdi <fengdi@tuniu.com>
2018-01-23 10:04:41 +0800

    作用域链使用对象拷贝完成，不使用数组

--- app/compile/components/for.js
+++ app/compile/components/for.js
@@ -5,14 +5,13 @@
  * @copyright Copyright &copy; 2006-2017 Tuniu.com
  */
 
-let utils = require();
+let utils = require('./../utils');
 let compile = require('./../index');
 let value = require('./value');
 
 module.exports = (list = [], json) => {
 	let head = list.shift();
 	let array = value(head[2], json) || [];
-	let scopeChain = [json];
 
 	return array.map((item, index) => {
 
